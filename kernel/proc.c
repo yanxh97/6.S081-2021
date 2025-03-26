2d52d4046c80b985f06d074a1b95029cea05f789
Xuehai Yan <yanxh15@gmail.com>
2025-03-25 21:20:57 -0700

    part 1 speed up getpid syscall

--- kernel/proc.c
+++ kernel/proc.c
@@ -126,6 +126,14 @@ found:
     release(&p->lock);
     return 0;
   }
+  
+  if((p->pusyscall = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+
+  p->pusyscall->pid = p->pid;
 
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
@@ -149,7 +157,11 @@ found:
 // p->lock must be held.
 static void
 freeproc(struct proc *p)
-{
+{  
+  if (p->pusyscall)
+    kfree((void*)p->pusyscall);
+  p->pusyscall = 0;
+  
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
@@ -196,6 +208,15 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+
+  if(mappages(pagetable, USYSCALL, PGSIZE, 
+              (uint64)(p->pusyscall), PTE_R | PTE_U) < 0){
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -206,6 +227,8 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
+  
   uvmfree(pagetable, sz);
 }
 
